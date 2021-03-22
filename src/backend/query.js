45d3e2e1573b6981ddedb92e3920f5862c191dc3
Yash Garg <yash.garg.cd.ece19@itbhu.ac.in>
2021-03-22 23:27:31 +0530

    query func updated

--- src/backend/query.js
+++ src/backend/query.js
@@ -2,6 +2,15 @@ import { db } from './database';
 
 var catalogRef = db.collection('catalog');
 
-export function searchObject(name) {
-    return catalogRef.where('object_name', '==', name).get();
+
+/* Asynchronous function returns a Promise
+ * Promise resolves into an object containing the data
+ */
+export async function searchObject(name) {
+    const query = await catalogRef.where('object_name', '==', name).get();
+    if (query.size) {
+        return query.docs[0].data();
+    } else {
+        return null;
+    }
 }
