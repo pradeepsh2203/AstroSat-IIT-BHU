6a9a590a4aa7abb551842d4babe85e1a0afde7e9
Yash Garg <yash.garg.cd.ece19@itbhu.ac.in>
2021-03-24 11:28:50 +0530

    created state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Query from "./components/Query";
 import Aladin from "./components/Aladin";
 import "tailwindcss/tailwind.css";
 
 function App() {
+
+  const [objects, setObjects] = useState([]);
+
   return (
     <div className="lg:grid lg:grid-cols-12">
       <div className="col-span-7">
-        <Query />
-        <Aladin />
+        <Query setObjects={setObjects} />
+        <Aladin objects={objects} />
       </div>
       <div className="bg-gray-100 rounded-lg rounded-r-none p-2 col-span-5 min-h-screen min-w-min">
         <h1 className="">Table</h1>
