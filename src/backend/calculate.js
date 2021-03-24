2e8b65a323e1d91b7e3ad7969262d0492bc36f6d
Yash jain <jainism987e@gmail.com>
2021-03-24 16:10:38 +0530

    update more functions

--- src/backend/calculate.js
+++ src/backend/calculate.js
@@ -1,3 +1,8 @@
+// Convert degree to radian
+function DegreeToRadian(degree) {
+    return degree * (Math.PI / 180);
+}
+
 /* 
 This function is used to calculate other celestial bodies in the range of search celestial body.
 It take the objects of search range query from database, distance to calculate and the seacrhed object.
@@ -6,12 +11,7 @@ it check each and every object with the searched object with the distance and re
 theta = cos-1(sin(delta1) * sin(delta2) + cos(delta1) * cos(delta2) * (cos(alpha1 - alpha2))
 where, right ascension(ra), alpha E [0, 2*pie], and declination (dec), delta E [-pie / 2, pie / 2]
 */
-
-function DegreeToRadian(degree) {
-    return degree * (Math.PI / 180);
-}
-
-function checkDistance(objects, alpha1, delta1, distance) {
+export function checkDistance(objects, alpha1, delta1, distance) {
     var insideRange = [];
 
     for (var i = 0; i < objects.length(); i++) {
@@ -24,4 +24,39 @@ function checkDistance(objects, alpha1, delta1, distance) {
     }
 
     return insideRange;
+}
+
+// Convert right ascension(ra) in hours, min, sec
+export function ConvertRAtohms(ra) {
+    var hours = ra * (24 / 360);
+    var min = hours * 60;
+    var sec = min * 60;
+
+    return [Math.floor(hours), Math.floor(min), Math.floor(sec)];
+}
+
+// Convert right ascension from hms to degree 
+export function ConverthmsToRA(hours, min, sec) {
+    var totalHours = hours + (min / 60) + (sec / 3600);
+    return (360 / 24) * totalHours;
+}
+
+// Convert declination (dec) to degree, arcmin, arcsec
+export function ConvertDECtodms(dec) {
+    return [Math.floor(dec), Math.floor(dec * 60), Math.floor(dec * 3600)];
+}
+
+// Convert declination (dec) from degree, arcmin, arcsec to degree
+export function ConvertdmstoDEC(degree, arcmin, arcsec) {
+    var negDegree = false;
+    if (degree < 0) {
+        degree = Math.abs(degree);
+        negDegree = true;
+    }
+
+    if (negDegree) {
+        return -(degree + (arcmin / 60) + (arcsec / 3600));
+    } else {
+        return (degree + (arcmin / 60) + (arcsec / 3600));
+    }
 }
